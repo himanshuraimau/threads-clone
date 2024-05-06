8dea6a7c8f3137be712b55000bd4949835da5fdd
Himanshu Rai <1si23ad016@sit.ac.in>
2024-05-06 10:09:12 +0530

    add UserPage PostPage

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css';
+import { Container } from '@chakra-ui/react';
+import { Routes, Route } from 'react-router-dom';
+import {UserPage} from './pages/UserPage.jsx';
+import {PostPage} from './pages/PostPage.jsx';
+import {Header} from './components/Header.jsx';
+
+function App() {
+  return (
+    <Container maxW='620px'>
+      <Header/>
+      <Routes>
+        <Route path='/:username' element={<UserPage/>} />
+        <Route path='/:username/post/:pid' element={<PostPage/>} />
+      </Routes>
+    </Container>
+  );
+}
+
+export default App;
